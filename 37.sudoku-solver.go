3ae9a11325d3441582a8db6f6d6583548c3393c2
20180014 <dack_huang@163.com>
2019-04-17 12:19:04 +0800

    20190417

--- 37.sudoku-solver.go
+++ 37.sudoku-solver.go
@@ -41,5 +41,37 @@
  *
  */
 func solveSudoku(board [][]byte) {
+	helper(board, 0, 0)
+}
 
+func helper(board [][]byte, row, col int) bool {
+	if col == len(board[0]) {
+		row++
+		col = 0
+	}
+	if row == len(board) {
+		return true
+	}
+	if board[row][col] != '.' {
+		return helper(board, row, col+1)
+	}
+	for ch := '1'; ch <= '9'; ch++ {
+		if invlid(board, row, col, byte(ch)) {
+			board[row][col] = byte(ch)
+			if helper(board, row, col+1) {
+				return true
+			}
+			board[row][col] = '.'
+		}
+	}
+	return false
+}
+
+func invlid(board [][]byte, row, col int, ch byte) bool {
+	for i := 0; i < 9; i++ {
+		if board[row][i] == ch || board[i][col] == ch || board[row/3*3+i/3][col/3*3+i%3] == ch {
+			return false
+		}
+	}
+	return true
 }
